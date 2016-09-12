514f92941ccc95f1a75e101516db243ee7169be2
Jakukyo Friel <weakish@gmail.com>
2016-09-12 22:24:38 +0800

    Use `end` as delimiter.

--- braces/register_codec.py
+++ braces/register_codec.py
@@ -1,7 +1,7 @@
 import codecs, cStringIO, encodings, tokenize
 from encodings import utf_8
 
-class BracesStreamReader(utf_8.StreamReader):
+class EndStreamReader(utf_8.StreamReader):
     def __init__(self, *args, **kwargs):
         codecs.StreamReader.__init__(self, *args, **kwargs)
 
@@ -16,33 +16,29 @@ class BracesStreamReader(utf_8.StreamReader):
         # Ignore indenting
         if tok_type in (tokenize.INDENT, tokenize.DEDENT):
             return
-        # Ignore `:`
-        if tok_type == tokenize.OP and tok_str == ':':
-            return
 
-        if tok_type == tokenize.COMMENT:
-            if tok_str == '#{' or tok_str == '# {':
-                tok_type = tokenize.INDENT
-                self._indent += 1
-                tok_str = '  ' * self._indent
-                self._tokens.append((tokenize.COLON, ':'))
-            elif tok_str == '#}' or tok_str == '# }':
-                tok_type = tokenize.DEDENT
-                self._indent -= 1
+        if tok_type == tokenize.OP and tok_str == ':':
+            tok_type = tokenize.INDENT
+            self._indent += 1
+            tok_str = '  ' * self._indent
+            self._tokens.append((tokenize.COLON, ':'))
+        elif tok_type == tokenize.NAME and tok_str == 'end':
+            tok_type = tokenize.DEDENT
+            self._indent -= 1
 
         self._tokens.append((tok_type, tok_str))
 
 def search_function(encoding):
-    if encoding != 'braces': return None
+    if encoding != 'end': return None
     # Assume utf8 encoding
     utf8 = encodings.search_function('utf8')
     return codecs.CodecInfo(
-        name = 'braces',
+        name = 'end',
         encode = utf8.encode,
         decode = utf8.decode,
         incrementalencoder = utf8.incrementalencoder,
         incrementaldecoder = utf8.incrementaldecoder,
-        streamreader = BracesStreamReader,
+        streamreader = EndStreamReader,
         streamwriter = utf8.streamwriter)
 
 codecs.register(search_function)
--- examples/noindent.py
+++ examples/noindent.py
@@ -1,16 +1,16 @@
-# coding: braces
+# coding: end
 
-def foo(x, y) #{
+def foo(x, y):
 return x + y
-#}
+end
 
-def main() #{
-for i in range(10) #{
+def main():
+for i in range(10):
 print "%s, %s" % (i * 2,
 i * 3)
-#}
-#}
+end
+end
 
-if __name__ == '__main__' #{
+if __name__ == '__main__':
 main()
-#}
+end
